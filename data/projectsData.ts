b67a4456d17b8330b52fb84718eae1a1daf208ea
Umair Anwar <umair.anwr@gmail.com>
2023-10-19 23:20:39 +0500

    Add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,36 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'A Learning Platform',
+    description: `Technology has powered the world and transformed a generation. At StackwisR, we embrace the information age with solutions to help you build and thrive in the ever-expanding tech industry. We focus on end results by helping our learners build exceptional careers along the world’s fastest-growing career paths.`,
+    imgSrc: '/static/images/sw.png',
+    href: 'https://www.stackwisr.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    title: 'App Maker For iOS & Android ',
+    description: `BuildFire’s powerful and easy to use mobile app builder makes it so you can create mobile apps for iOS & Android in a fraction of the time and cost.`,
+    imgSrc: '/static/images/buildfire.png',
     href: '/blog/the-time-machine',
   },
+  {
+    title: 'A Customer Service Platform',
+    description: `Get a frictionless customer service platform. Across live chat, email and social. Automate workflows. Collaborate with your team from a shared inbox. See the whole customer journey from a single view. For SMBs looking for a fully integrated cloud-hosted help desk to immediately start supporting customers.`,
+    imgSrc: '/static/images/kayako.png',
+    href: 'https://kayako.com/',
+  },
+  {
+    title: 'A TV Delivery Platform',
+    description: `Our pioneering solutions help Pay TV operators navigate the challenges of an all-IP broadcast future. Our experience in Pay TV has allowed us to develop solutions to securely deliver cutting-edge audio, video, and graphics solutions in any commercial environment, making us a preferred partner for Digital Signage and Enterprise Video system integrators and technology partners.`,
+    imgSrc: '/static/images/aminocom.png',
+    href: 'https://www.amino.tv/',
+  },
+  {
+    title: 'Improve your rowing technique',
+    description: `The Quiske App + pod visualizes your performance. The analyzed data is available immediately to visualize your stroke and help make the next one better. Instant feedback enables fast learning. Effortless measurement makes it possible to track performance in every rowing session. Bring confidence to your coaching by getting concrete facts on rowing technique.`,
+    imgSrc: '/static/images/quiske.png',
+    href: 'https://www.rowingperformance.com/',
+  }
+
+
 ]
 
 export default projectsData
