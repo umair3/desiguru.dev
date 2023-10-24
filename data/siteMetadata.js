0e67bf06b5be2aef47ab37f444193f735cff514c
Umair Anwar <umair.anwr@gmail.com>
2023-10-24 08:40:38 +0500

    Add siteUrl and siteRepo in siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'Purpose Driven Learning',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://desiguru-dev.vercel.app',
+  siteRepo: 'https://github.com/umair3/desiguru.dev',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
